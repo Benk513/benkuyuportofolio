b5d777352559d2154f982bdbe5054b61f2618ad0
Benk513 <79744201+Benk513@users.noreply.github.com>
2025-01-16 16:34:20 +0100

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,13 @@ export default {
   theme: {
     extend: {
       colors: {
+        black:{
+          DEFAULT :'#000',
+          100:'#000319'
+        },
         background: "var(--background)",
         foreground: "var(--foreground)",
+
       },
     },
   },
