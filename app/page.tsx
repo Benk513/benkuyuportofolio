860dc30853d9db04bf8ea04e892fd86247da1a70
Benk513 <79744201+Benk513@users.noreply.github.com>
2025-01-21 16:39:03 +0100

    add testimonial section

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,12 @@ import Hero from "./components/Hero";
 import { FloatingNav } from "./components/ui/FloatingNav";
 import Grid from "./components/ui/Grid";
 import RecentProjects from "./components/ui/RecentProjects";
+import Clients from "./components/ui/Clients";
 
 export default function Home() {
   return (
    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
-    <div className=" max-w-7xl w-full">
+    <div className=" max-w-6xl w-full">
       <FloatingNav
       navItems= {[
         {
@@ -22,6 +23,7 @@ export default function Home() {
        <Hero/>
        <Grid/>
        <RecentProjects/>
+       <Clients/>
     </div>
    </main>
       
